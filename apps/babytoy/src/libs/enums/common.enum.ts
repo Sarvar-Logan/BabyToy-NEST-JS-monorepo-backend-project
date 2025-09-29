cf62021aded0c24efdadee955636332d96114d2c
Sarvar-Logan <sarvartojeboev@gmail.com>
2025-09-29 17:02:01 +0900

    feat: buil project standarts

---
+++ apps/babytoy/src/libs/enums/common.enum.ts
@@ -0,0 +1,22 @@
+export enum Message  {
+
+    SOMETHING_WENT_WRONG =  "Something went wrong", 
+    NO_DATA_FOUND = "No data is found", 
+    CREATE_FAILED = "Create is failed",
+    UPDATE_FAILED = "Update is failed",
+    REMOVE_FAILED = "Remove failed",
+    UPLOAD_FAILED = "Upload is failed",
+    BAD_REQUEST = "Bad request",    
+
+    NO_MEMBER_NICK = "No member with that nickname",
+    BLOCKED_USER = "You have been blocked!",
+    WRONG_PASSWORD = "Wrong Password entered, please try again",
+    NOT_AUTHENTICATED = "You are not authentiacted, Please login firs!",
+    TOKEN_NOT_EXIST = " Berer Token is not provided!",
+    ONLY_SPESIFIC_ROLES_ALLOWED = "Allowed only for members wuith spesific role",
+    NOT_ALOWED_REQUEST = "Not allowed request",
+    PROVIDE_ALLOWED_FORMAT = "Please provide jpg, jpeg, or pnj images",
+    SELF_SUBSCRIPTION_DENIED = "Self subscription is denied",
+
+  
+}
