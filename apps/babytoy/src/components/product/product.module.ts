fa69f69122e529af22ef9cc4bff62e1ab5ab7e16
Sarvar-Logan <sarvartojeboev@gmail.com>
2025-09-30 00:42:24 +0900

    feat: create product and member module related service and resolvers

--- apps/babytoy/src/components/product/product.module.ts
+++ apps/babytoy/src/components/product/product.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ProductResolver } from './product.resolver';
+import { ProductService } from './product.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import ProductSchema from '../../schemas/Product.model';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forFeature([{name: "Product", schema: ProductSchema}])],
+  providers: [ProductResolver, ProductService]
+})
 export class ProductModule {}
