311fb6b8809ffaeb9a136880b5feb1935a88c7f7
Sarvar-Logan <sarvartojeboev@gmail.com>
2025-10-03 02:05:28 +0900

    feat: create boardArticles related graphql APIs

--- apps/babytoy/src/components/board-article/board-article.module.ts
+++ apps/babytoy/src/components/board-article/board-article.module.ts
@@ -5,12 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import BoardArticleSchema from '../../schemas/BoardArticle.model';
 import { AuthModule } from '../auth/auth.module';
 import { MemberModule } from '../member/member.module';
+import { ViewModule } from '../view/view.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: "BoardArticle", schema: BoardArticleSchema}]), 
     AuthModule, 
-    MemberModule
+    MemberModule,
+    ViewModule
   ],
   providers: [BoardArticleResolver, BoardArticleService]
 })
